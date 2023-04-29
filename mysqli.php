b7b22f0d2918acd4a516112653c615c9b96a0ff0
Adrián Iglesias Fernández <72018196+IgleDev@users.noreply.github.com>
2023-04-29 04:20:54 +0200

    Update mysqli.php

--- mysqli.php
+++ mysqli.php
@@ -1,8 +1,8 @@
 <?php 
-$server = 'containers-us-west-9.railway.app';
+$server = 'localhost';
 $user = 'root';
-$pass = 'uUp4KnwvfZyI5mg21sFO';
-$dataBase = 'railway';
+$pass = '';
+$dataBase = 'agenda2';
 
 $mysqli = mysqli_connect($server, $user, $pass, $dataBase) or die(mysqli_connect_error());
 ?>
